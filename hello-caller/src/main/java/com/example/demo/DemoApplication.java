eb2d756e7d906d561437bad51ba4f25bccd1d4c0
Phan Tiến Đạt <126739355+DatPhan06@users.noreply.github.com>
2025-03-27 02:39:11 +0700

    Hello World V1

--- hello-caller/src/main/java/com/example/demo/DemoApplication.java
+++ hello-caller/src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootApplication
 @RestController
@@ -13,8 +15,19 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public String index() {
-		return "Hello World";
+	@GetMapping("/")
+	public ResponseEntity<String> index() {
+		return ResponseEntity.ok("Hello World from root endpoint");
+	}
+
+	@GetMapping("/test")
+	public ResponseEntity<String> test() {
+		return ResponseEntity.ok("Hello World from test endpoint");
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleException(Exception e) {
+		return ResponseEntity.internalServerError()
+				.body("Có lỗi xảy ra: " + e.getMessage());
 	}
 }
